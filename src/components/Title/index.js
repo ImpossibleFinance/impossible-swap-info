a40d691ea5d41e2d51a1df9782c54dbdfff78d6b
bokayi <stablexdevO2@outlook.com>
2021-05-04 22:50:31 +0800

    feat: inital commit

---
+++ src/components/Title/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { Flex } from 'rebass'
+import Link from '../Link'
+import { RowFixed } from '../Row'
+
+const TitleWrapper = styled.div`
+  text-decoration: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  z-index: 10;
+`
+
+const UniIcon = styled(Link)`
+  transition: transform 0.3s ease;
+  :hover {
+    transform: rotate(-5deg);
+  }
+`
+
+export default function Title() {
+  const history = useHistory()
+
+  return (
+    <TitleWrapper onClick={() => history.push('/')}>
+      <Flex alignItems="center">
+        <RowFixed>
+          <UniIcon id="link" onClick={() => history.push('/')}>
+            <img width={'150px'} src="https://impossible.finance/images/logo.svg" alt="logo" />
+          </UniIcon>
+        </RowFixed>
+      </Flex>
+    </TitleWrapper>
+  )
+}
