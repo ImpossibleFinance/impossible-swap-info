aae960e3e593160ae67aa2d14dbe72504de69d09
Accelerated Monkey <acceleratedape@protonmail.com>
2021-08-18 18:00:47 +0800

    update graph endpoing

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.bscgraph.org/subgraphs/name/imfswap',
+    uri: 'https://api.thegraph.com/subgraphs/name/amanraj1608/uni-sub',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -12,7 +12,7 @@ export const client = new ApolloClient({
 
 export const healthClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/graphql',
+    uri: 'https://api.thegraph.com/index-node/graphql',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "te") {
+    indexingStatusForCurrentVersion(subgraphName: "amanraj1608/uni-sub") {
       synced
       health
       chains {
