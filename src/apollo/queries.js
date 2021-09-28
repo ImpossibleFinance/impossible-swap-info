9ab3b924e383e7c8973c3e60b41f473045bc1855
Aman <archanaamanraj@gmail.com>
2021-09-28 17:28:43 +0530

    revert to old subgraph

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "amanraj1608/uni-sub") {
+    indexingStatusForCurrentVersion(subgraphName: "te") {
       synced
       health
       chains {
